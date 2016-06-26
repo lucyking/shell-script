ad247e6e813cd764ebd88819c0a0d454d20f4f53
lucyking <lucyking@users.noreply.github.com>
2016-06-26 22:06:44 +0800

    using ${var}
    var is better

--- lec.sh
+++ lec.sh
@@ -4,9 +4,9 @@ echo ' '
 var=`pwd | sed -e 's/ /_/g;s/\.//g' | xargs basename`
 echo ${PWD}'/'`ls|grep *.cpp`  
 echo " --- ---> "
-echo "/root/leetcode/leetcode/c/`pwd | sed -e 's/ /_/g;s/\.//g' | xargs basename`.cpp"
-cp main.cpp /root/leetcode/leetcode/c/`pwd | sed -e 's/ /_/g;s/\.//g' | xargs basename`.cpp
+echo "/root/leetcode/leetcode/c/${var}.cpp"
+cp main.cpp /root/leetcode/leetcode/c/${var}.cpp
 echo ' '
 cd /root/leetcode/leetcode/c/
-git add *
+#git add *
 #git ci -m +${var}
